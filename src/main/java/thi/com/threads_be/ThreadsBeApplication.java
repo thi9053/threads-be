2da237e2a7670bcaa47db782acbfd995c48a3de9
thi9053 <levanthi.tl1211@proton.me>
2025-06-07 21:21:31 +0700

    Init project

---
+++ src/main/java/thi/com/threads_be/ThreadsBeApplication.java
@@ -0,0 +1,13 @@
+package thi.com.threads_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsBeApplication.class, args);
+	}
+
+}
